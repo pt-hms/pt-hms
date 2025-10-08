5b27535d4d4ede9a1d153124c7fb054e43304cee
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2025-10-08 17:18:52 +0700

    init project

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+import withPWA from "next-pwa";
+
+const nextConfig = {};
+
+const config = withPWA({
+   dest: "public",
+   register: true,
+   skipWaiting: true,
+   disable: process.env.NODE_ENV === "development",
+})(nextConfig);
+
+export default nextConfig;

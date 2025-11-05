4a74f5b0edcedea93c108b0e60c8ec7ba19c814a
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2025-11-05 09:38:40 +0700

    update backend url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.API_URL || "https://api.pt-hms.com/api",
+    baseURL: process.env.API_URL || 'https://apipthms.vercel.app/' || "https://api.pt-hms.com/api",
 });
 
 // Tambahkan token dari cookie secara otomatis
